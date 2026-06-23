aae6a288c90334158eedbeedc901d629202bf5c8
thmedeiros15 <rastrekbr@gmail.com>
2026-06-23 12:16:02 -0300

    fix: change vite base path to root for custom domain support

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/site-falcontech/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
